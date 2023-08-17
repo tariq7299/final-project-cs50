47951a1d97141ddb9dfa6e8d418913f2044b0c9d
Tariq Mohamed Sarhan <tr.sar77an78@gmail.com>
2023-08-17 11:56:25 +0300

    test2

--- app.py
+++ app.py
@@ -2,3 +2,15 @@ print("hello world")
 print("hello world")
 print("hello world")
 print("hello world")
+
+
+def validate_password(password):
+    import re # regular expression
+    if len(password) < 8:
+        return ("Password should be at least 8 characters or longer")
+    elif not re.search("[0-9]", password):
+        return ("Password must contain at least one digit")
+    elif not re.search("[A-Z]", password):
+        return ("Password must contain at least one uppercase letter")
+    elif not re.search("[@_!#$%&^*()<>?~+-/\{}:]",password):
+        return ("password must contain at least one special charaster")
